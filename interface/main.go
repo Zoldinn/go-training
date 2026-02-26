f64117921a765236ec25a2695933c3e40292156e
zoldinn <lefoffan@student.42perpignan.fr>
2026-02-26 17:26:41 +0100

    interface in go seems very usefull

--- interface/main.go
+++ interface/main.go
@@ -5,42 +5,35 @@ import "fmt"
 // ===================================
 
 type Animal interface {
-	Parler() string
+	Parler()
 }
 
-func FaireParler(a Animal) string {
-	return a.Parler()
-}
+// func FaireParler(a Animal) {
+// 	a.Parler()
+// }
 
 // ===================================
 
-type Chien struct {
-	name string
-}
+type Chien struct { name string }
 
-func (c Chien) Parler() string {
-	return "Woof woof !"
-}
+func (c Chien) Parler() { fmt.Printf( "%v : Woof woof !\n", c.name ) }
 
 // ===================================
 
-func (c Chat) Parler() string {
-	return "Miaow Miaow !"
-}
+type Chat struct { name string }
 
-type Chat struct {
-	name string
-}
+func (c Chat) Parler() { fmt.Printf( "%v : Miaow Miaow !\n", c.name ) }
 
 // ===================================
 
 func main() {
-	var a Animal
-	chien := Chien{"boby"}
-	chat := Chat{"toto"}
 
-	a = chien
-	fmt.Println( FaireParler(a) )
-	a = chat
-	fmt.Println( FaireParler(a) )
+	objs := []Animal {
+		Chien{"Medor"},
+		Chat{"Nemo"},
+	}
+
+	for _, obj := range objs {
+		obj.Parler()
+	}
 }
